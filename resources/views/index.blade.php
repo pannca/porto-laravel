3eaf64b83b72ae6b32367a96c302d2738b52098d
pannca <pancaprasetia@smkwikrama.sch.id>
2024-06-13 19:26:34 +0700

    upload

---
+++ resources/views/index.blade.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PORTOFOLIO</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="responsive.css">
+</head>
+
+<body>
+        <header>
+            <div class="logo" id="homee">Panca</div>
+            <nav>
+                <ul>
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
+            </nav>
+        </header>
+
+        <section class="home" id="home">
+                <img src="levi.jpeg" alt="">
+                <div class="texthome">
+                <h1>Hello, saya <br>Panca Prasetia</h1>
+                <p>this is my portofolio</p>
+                <button><a href="">let's goo</a></button>
+                </div>
+        </section>
+
+         <section class="about" id="about" >
+                <h1 >ABOUT ME</h1>
+                <div style="justify-content: center;" class="textabout"> 
+                <p>Hai semuanya. Izinkan saya untuk perkenalkan diri saya. Nama saya Panca Prasetia, panggil saya Panca. Saya lahir di Bogor. Saat ini saya sedang sekolah di SMK WIKRAMA BOGOR. Di samping itu, saya juga sedang fokus mengulik front end . Semoga impian saya dapat tercapai. Itu yang dapat saya sampaikan di perkenalan ini. Terima kasih dan semoga harimu menyenangkan!</p>
+                <img src="luffy.jpg" alt="">
+                </div>
+        </section>
+  
+        <section class="contact" id="contact">
+            <div class="judulcontact">CONTACT</div>
+            <p>ini adalah sosmed saya</p>
+            <div class="bungkuscontact">
+
+            <div class="contact1">
+            <img src="instagram.png" alt="">
+            <h1>instagram</h1>
+            <p><a href="https://www.instagram.com/pannca24/">Klik Disini</a></p>
+            </div>
+            
+            <div class="contact2">
+            <img src="whatsapp.png" alt="">
+            <h1>whatsaap</h1>
+            <p><a href="https://wa.me/6285717618772">Klik Disini</a></p>
+            </div>
+
+            <div class="contact3">
+            <img src="github-sign.png" alt="">
+            <h1>github</h1>
+            <p><a href="https://github.com/pannca">Klik Disini</a></p>
+            </div>
+
+            </div>
+        </section> 
+</body>
+
+</html>
